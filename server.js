e2059b7df2bea02d8fe0ba8ddfc3e76e837f9e80
Makrem Ltifi <makrem.ltifi.77@gmail.com>
2025-04-28 23:35:23 +0000

    finish the mission

--- server.js
+++ server.js
@@ -3,55 +3,32 @@ const express     = require('express');
 const bodyParser  = require('body-parser');
 const fccTesting  = require('./freeCodeCamp/fcctesting.js');
 const app         = express();
-const bcrypt      = require('bcrypt');
+const bcrypt = require('bcrypt');
 
-fccTesting(app);
+fccTesting(app); //For FCC testing purposes
+
 const saltRounds = 12;
 const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
-
-
-//START_ASYNC -do not remove notes, place code between correct pair of notes.
-
-
-
+   
+//START_ASYNC
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+  console.log(hash);
+  bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+    console.log(res);
+  });
+});
 //END_ASYNC
 
 //START_SYNC
 
-
-
+ let hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+console.log(hash);
+let result = bcrypt.compareSync(myPlaintextPassword, hash);
+console.log(result);
 //END_SYNC
 
 
+    
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log("Listening on port:", PORT)
-});
+app.listen(process.env.PORT || 3000, () => {});
